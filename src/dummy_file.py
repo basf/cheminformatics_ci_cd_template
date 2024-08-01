b97f3cce8cb98341da1361712f8f44472eda2377
Jochen Sieg <jochen.sieg@basf.com>
2024-08-01 10:32:20 +0200

    add dummy function with test

---
+++ src/dummy_file.py
@@ -0,0 +1,20 @@
+"""A dummy file to test the CI/CD template"""
+
+import numpy.typing as npt
+from scipy.sparse import csr_matrix, spmatrix
+
+
+def dense_to_sparse(matrix: npt.NDArray) -> spmatrix:
+    """Convert a dense matrix to a sparse matrix.
+
+    Parameters
+    ----------
+    matrix : npt.NDArray
+        A dense matrix.
+
+    Returns
+    -------
+    spmatrix
+        A sparse matrix.
+    """
+    return csr_matrix(matrix)
---
+++ tests/test_dummy_file.py
@@ -0,0 +1,20 @@
+"""Test for the dummy file."""
+
+import unittest
+
+import numpy as np
+from scipy.sparse import issparse
+
+from src.dummy_file import dense_to_sparse
+
+
+class TestDummyFile(unittest.TestCase):
+    """Tester for the dummy file."""
+
+    def test_dense_to_sparse(self):
+        """Test the dense_to_sparse function."""
+
+        matrix = np.array([[1, 0, 0], [0, 0, 1]])
+        sparse_matrix = dense_to_sparse(matrix)
+        self.assertTrue(issparse(sparse_matrix))
+        self.assertTrue((sparse_matrix.toarray() == matrix).all())
